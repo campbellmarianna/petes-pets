d1d51519c2416ef4e058f2e55f374e901f5ca338
Marianna <mckd2018@gmail.com>
2019-04-15 08:21:10 -0700

    [feat] Implemented responding to JSON

--- test/test-pets.js
+++ test/test-pets.js
@@ -129,5 +129,17 @@ describe('Pets', ()  => {
         });
   });
 
-  
+  // INDEX - JSON RESPONSE
+  it('sould list ALL pets on /pets GET', function(done) {
+      chai.request(server)
+        .get('/')
+        .set('content-type', 'application/json')
+        .end(function(err, res){
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            done();
+        });
+  })
+
 });
